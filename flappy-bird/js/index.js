1d9029bfb278a32519467d5ca28802d434da5cee
Francisco Madruga <madruga.francisco@protonmail.com>
2023-08-15 20:07:45 +0100

    responsive

--- flappy-bird/js/index.js
+++ flappy-bird/js/index.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("canvas");
+
+if (navigator.userAgentData.mobile) {
+  canvas.height = innerHeight - 20;
+  canvas.width = innerWidth - 20;
+} else {
+  canvas.height = 1000;
+  canvas.width = 700;
+}
+
 const ctx = canvas.getContext("2d");
 
 const game = new Game(canvas, ctx, BACKGROUND_COLOR);
